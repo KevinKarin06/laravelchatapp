68fdebe0328760a77fd3dd51807acd11bc17020e
KevinKarin06 <55352660+KevinKarin06@users.noreply.github.com>
2020-01-11 11:41:25 +0100

    added chat view

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+</head>
+<body>
+    <div class="container">
+        <div class="row" id="app">
+            <div class="offset-4 col-md-4 col-sm-12">
+                <li class="list-group-item active">Chat Room</li>
+                <ul class="list-group">
+                <message
+                    v-for="value in chat.message">
+                    @{{value}}
+                </message>
+                  </ul>
+                  <input type="text" class="form-control" placeholder="Type your message here..."
+                    v-model='message' @keyup.enter='send'>
+            </div>
+
+        </div>
+    </div>
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
